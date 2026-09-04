959b16a7376674384b172ac80afafd930edca421
onepointfive <122128996+onepointfive-REAL@users.noreply.github.com>
2026-09-03 19:12:22 -0500

    Delete shortcut_browser.sh

--- shortcut_browser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Name: FAAAAAAAAA
-# Author: shaaaa
-
-# Launch the native browser pointing to your target URL
-lipc-set-prop com.lab126.appmgrd start "app://com.lab126.browser#going?url=https://google.com"
